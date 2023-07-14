8de9526496f563c5f99695939d03901617000a87
Chiara <93772885+filesbychiara@users.noreply.github.com>
2023-07-14 19:38:16 +0100

    implemented the unit conversion

--- src/index.js
+++ src/index.js
@@ -24,12 +24,16 @@ function formatDate(date) {
 }
 
 function displayWeatherCondition(response) {
+  let temperatureElement = document.querySelector("#temperature");
   let dateElement = document.querySelector("#date");
   let currentTime = new Date();
   let searchForm = document.querySelector("#search-bar");
   let currentLocationButton = document.querySelector("#current-location-btn");
   let iconElement = document.querySelector("#icon");
   
+  celsiusTemperature = response.data.main.temp;
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
@@ -70,16 +74,25 @@ function getCurrentLocation(event) {
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
-function convertToFahrenheit(event) {
+function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 66;
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
 }
 
-function convertToCelsius(event) {
+function displayCelsiusTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 19;
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
+
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("Hong Kong");
